755065030d9cf26bfb09cea28f5e2e661d66795a
Andrei Olar <olarandrei@gmail.com>
2016-12-04 23:39:01 +0200

    [GENERALDEV-5] Redesigning Application based on Material Design

--- Abms/src/main/java/com/andreiolar/abms/client/view/impl/LoginViewImpl.java
+++ Abms/src/main/java/com/andreiolar/abms/client/view/impl/LoginViewImpl.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import com.andreiolar.abms.client.constants.DialogBoxConstants;
 import com.andreiolar.abms.client.constants.LoginFormConstants;
 import com.andreiolar.abms.client.place.AdminPlace;
-import com.andreiolar.abms.client.place.RegisterPlace;
 import com.andreiolar.abms.client.place.UserPlace;
 import com.andreiolar.abms.client.rpc.DBConnection;
 import com.andreiolar.abms.client.rpc.DBConnectionAsync;
@@ -15,35 +14,44 @@ import com.andreiolar.abms.client.utils.DialogBoxCreator;
 import com.andreiolar.abms.client.view.LoginView;
 import com.andreiolar.abms.shared.User;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.dom.client.Style.Display;
+import com.google.gwt.dom.client.Style.Float;
+import com.google.gwt.dom.client.Style.FontStyle;
+import com.google.gwt.dom.client.Style.TextDecoration;
+import com.google.gwt.dom.client.Style.TextTransform;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyDownEvent;
-import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.DisclosurePanel;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.FlexTable.FlexCellFormatter;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+
+import gwt.material.design.client.constants.ButtonType;
+import gwt.material.design.client.constants.Color;
+import gwt.material.design.client.constants.ImageType;
+import gwt.material.design.client.constants.InputType;
+import gwt.material.design.client.constants.WavesType;
+import gwt.material.design.client.ui.MaterialButton;
+import gwt.material.design.client.ui.MaterialCheckBox;
+import gwt.material.design.client.ui.MaterialColumn;
+import gwt.material.design.client.ui.MaterialImage;
+import gwt.material.design.client.ui.MaterialPanel;
+import gwt.material.design.client.ui.MaterialRow;
+import gwt.material.design.client.ui.MaterialTextBox;
+import gwt.material.design.client.ui.MaterialToast;
 
 public class LoginViewImpl extends Composite implements LoginView {
 
@@ -57,142 +65,235 @@ public class LoginViewImpl extends Composite implements LoginView {
 	private static String name;
 
 	public LoginViewImpl() {
-
-		panel.setSpacing(8);
 		Widget loginForm = createLoginForm();
-		panel.add(loginForm);
-		panel.setStyleName("loginPanel");
 
-		initWidget(panel);
+		initWidget(loginForm);
 	}
 
-	@SuppressWarnings("deprecation")
-	private Widget createLoginForm() {
+	public Widget createLoginForm() {
+		MaterialRow materialRow = new MaterialRow();
+		materialRow.setBackgroundColor(Color.GREY_LIGHTEN_4);
+		materialRow.addStyleName("login-demo");
 
-		// Create table to layout the form options
-		FlexTable layout = new FlexTable();
-		layout.setCellSpacing(6);
-		layout.setWidth("300px");
-		FlexCellFormatter cellFormatter = layout.getFlexCellFormatter();
+		MaterialColumn materialColumn = new MaterialColumn(12, 8, 6);
+		materialColumn.setOffset("l3");
 
-		// Add a title to the form
-		layout.setHTML(0, 0, LoginFormConstants.FORM_NAME);
-		cellFormatter.setColSpan(0, 0, 2);
-		cellFormatter.setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
+		MaterialPanel panel = new MaterialPanel();
+		panel.setShadow(1);
+		panel.addStyleName("panel");
 
-		// Add some standard form options
-		layout.setHTML(1, 0, LoginFormConstants.USERNAME);
-		layout.setWidget(1, 1, usernameBox);
-		layout.setHTML(2, 0, LoginFormConstants.PASSWORD);
-		layout.setWidget(2, 1, passwordBox);
+		MaterialPanel fieldPanel = new MaterialPanel();
+		fieldPanel.addStyleName("fieldPanel");
 
-		usernameBox.setStyleName("fixed-input");
-		passwordBox.setStyleName("fixed-input");
+		MaterialImage materialImage = new MaterialImage("images/icons/ab.png");
+		materialImage.setType(ImageType.CIRCLE);
+		materialImage.addStyleName("imgProfile");
 
-		passwordBox.addKeyDownHandler(new KeyDownHandler() {
+		MaterialTextBox userBox = new MaterialTextBox();
+		userBox.setType(InputType.TEXT);
+		userBox.setPlaceholder("Username");
 
-			@Override
-			public void onKeyDown(KeyDownEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					final String username = usernameBox.getText();
-					final String password = passwordBox.getText();
-					GWT.runAsync(new RunAsyncCallback() {
+		MaterialTextBox passwordBox = new MaterialTextBox();
+		passwordBox.setType(InputType.PASSWORD);
+		passwordBox.setPlaceholder("Password");
 
-						@Override
-						public void onSuccess() {
-							performUserConnection(username, password);
-						}
-
-						@Override
-						public void onFailure(Throwable reason) {
-							// TODO Auto-generated method stub
-						}
-					});
-				}
-			}
-		});
-
-		// Create buttons
-		loginButton.setHTML(LoginFormConstants.LOGIN_BUTTON);
+		MaterialButton loginButton = new MaterialButton();
+		loginButton.setWaves(WavesType.LIGHT);
+		loginButton.setText("Log In");
+		loginButton.setWidth("100%");
 		loginButton.addClickHandler(new ClickHandler() {
 
 			@Override
 			public void onClick(ClickEvent event) {
-				final String username = usernameBox.getText();
-				final String password = passwordBox.getText();
-				GWT.runAsync(new RunAsyncCallback() {
-
-					@Override
-					public void onSuccess() {
-						performUserConnection(username, password);
-					}
-
-					@Override
-					public void onFailure(Throwable reason) {
-						// TODO Auto-generated method stub
-					}
-				});
+				MaterialToast.fireToast("Login", "rounded");
 			}
 		});
 
-		registerButton.setHTML(LoginFormConstants.REGISTER_BUTTON);
+		MaterialButton registerButton = new MaterialButton();
+		registerButton.setWaves(WavesType.LIGHT);
+		registerButton.setText("Register");
+		registerButton.setWidth("100%");
+		registerButton.getElement().getStyle().setMarginTop(20.0, Unit.PX);
+		registerButton.setBackgroundColor(Color.LIGHT_BLUE_LIGHTEN_3);
 		registerButton.addClickHandler(new ClickHandler() {
 
 			@Override
 			public void onClick(ClickEvent event) {
-				presenter.goTo(new RegisterPlace(name));
+				MaterialToast.fireToast("Register", "rounded");
 			}
 		});
 
-		// Create panel to hold buttons
-		HorizontalPanel hPanel = new HorizontalPanel();
+		MaterialRow rowAction = new MaterialRow();
+		rowAction.addStyleName("rowAction");
 
-		hPanel.setSpacing(10);
-		hPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_DEFAULT);
+		MaterialColumn mCol = new MaterialColumn(12, 12, 6);
+		mCol.setWidth("100%");
 
-		hPanel.add(loginButton);
-		hPanel.add(registerButton);
+		MaterialCheckBox loggedIn = new MaterialCheckBox();
+		loggedIn.setText("Keep me logged in");
 
-		// Add horizontal panel to layout
-		layout.setWidget(3, 0, hPanel);
-		cellFormatter.setColSpan(3, 0, 3);
-
-		// Create advanced options
-		Grid advancedOptions = new Grid(1, 1);
-		advancedOptions.setCellSpacing(6);
-
-		Hyperlink hyperlink = new Hyperlink();
-		hyperlink.setHTML(LoginFormConstants.HYPERLINK_TEXT);
-		hyperlink.addClickHandler(new ClickHandler() {
+		MaterialButton forgotPaswordLink = new MaterialButton();
+		forgotPaswordLink.setText("Forgot Password?");
+		forgotPaswordLink.setTextColor(Color.BLUE);
+		forgotPaswordLink.setType(ButtonType.FLAT);
+		forgotPaswordLink.getElement().getStyle().setMarginTop(10.0, Unit.PX);
+		forgotPaswordLink.setFloat(Float.RIGHT);
+		forgotPaswordLink.getElement().getStyle().setTextDecoration(TextDecoration.UNDERLINE);
+		forgotPaswordLink.getElement().getStyle().setTextTransform(TextTransform.CAPITALIZE);
+		forgotPaswordLink.getElement().getStyle().setFontStyle(FontStyle.ITALIC);
+		forgotPaswordLink.addClickHandler(new ClickHandler() {
 
 			@Override
 			public void onClick(ClickEvent event) {
-				DialogBox dialogBox = createForgotPasswordDialogBox();
-				dialogBox.setGlassEnabled(true);
-				dialogBox.setAnimationEnabled(true);
-				dialogBox.center();
-				dialogBox.show();
+				MaterialToast.fireToast("Password", "rounded");
 			}
 		});
-		advancedOptions.setWidget(0, 0, hyperlink);
 
-		// Add advanced options to form in a disclosure panel
-		DisclosurePanel advancedDisclosure = new DisclosurePanel(LoginFormConstants.ADVANCED_CRITERIA);
-		advancedDisclosure.setAnimationEnabled(true);
-		advancedDisclosure.ensureDebugId("disclosurePanel");
-		advancedDisclosure.setContent(advancedOptions);
+		mCol.add(loggedIn);
+		mCol.add(forgotPaswordLink);
+		rowAction.add(mCol);
 
-		// Add disclorue panel to layout
-		layout.setWidget(4, 0, advancedDisclosure);
-		cellFormatter.setColSpan(4, 0, 2);
+		fieldPanel.add(materialImage);
+		fieldPanel.add(userBox);
+		fieldPanel.add(passwordBox);
+		fieldPanel.add(loginButton);
+		fieldPanel.add(registerButton);
+		fieldPanel.add(rowAction);
 
-		// Wrap the contents in a DecoratorPanel
-		DecoratorPanel decPanel = new DecoratorPanel();
-		decPanel.setWidget(layout);
+		panel.add(fieldPanel);
+		materialColumn.add(panel);
+		materialRow.add(materialColumn);
 
-		return decPanel;
-
+		return materialRow;
 	}
+
+	// @SuppressWarnings("deprecation")
+	// private Widget createLoginForm() {
+	//
+	// // Create table to layout the form options
+	// FlexTable layout = new FlexTable();
+	// layout.setCellSpacing(6);
+	// layout.setWidth("300px");
+	// FlexCellFormatter cellFormatter = layout.getFlexCellFormatter();
+	//
+	// // Add a title to the form
+	// layout.setHTML(0, 0, LoginFormConstants.FORM_NAME);
+	// cellFormatter.setColSpan(0, 0, 2);
+	// cellFormatter.setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
+	//
+	// // Add some standard form options
+	// layout.setHTML(1, 0, LoginFormConstants.USERNAME);
+	// layout.setWidget(1, 1, usernameBox);
+	// layout.setHTML(2, 0, LoginFormConstants.PASSWORD);
+	// layout.setWidget(2, 1, passwordBox);
+	//
+	// usernameBox.setStyleName("fixed-input");
+	// passwordBox.setStyleName("fixed-input");
+	//
+	// passwordBox.addKeyDownHandler(new KeyDownHandler() {
+	//
+	// @Override
+	// public void onKeyDown(KeyDownEvent event) {
+	// if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+	// final String username = usernameBox.getText();
+	// final String password = passwordBox.getText();
+	// GWT.runAsync(new RunAsyncCallback() {
+	//
+	// @Override
+	// public void onSuccess() {
+	// performUserConnection(username, password);
+	// }
+	//
+	// @Override
+	// public void onFailure(Throwable reason) {
+	// // TODO Auto-generated method stub
+	// }
+	// });
+	// }
+	// }
+	// });
+	//
+	// // Create buttons
+	// loginButton.setHTML(LoginFormConstants.LOGIN_BUTTON);
+	// loginButton.addClickHandler(new ClickHandler() {
+	//
+	// @Override
+	// public void onClick(ClickEvent event) {
+	// final String username = usernameBox.getText();
+	// final String password = passwordBox.getText();
+	// GWT.runAsync(new RunAsyncCallback() {
+	//
+	// @Override
+	// public void onSuccess() {
+	// performUserConnection(username, password);
+	// }
+	//
+	// @Override
+	// public void onFailure(Throwable reason) {
+	// // TODO Auto-generated method stub
+	// }
+	// });
+	// }
+	// });
+	//
+	// registerButton.setHTML(LoginFormConstants.REGISTER_BUTTON);
+	// registerButton.addClickHandler(new ClickHandler() {
+	//
+	// @Override
+	// public void onClick(ClickEvent event) {
+	// presenter.goTo(new RegisterPlace(name));
+	// }
+	// });
+	//
+	// // Create panel to hold buttons
+	// HorizontalPanel hPanel = new HorizontalPanel();
+	//
+	// hPanel.setSpacing(10);
+	// hPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_DEFAULT);
+	//
+	// hPanel.add(loginButton);
+	// hPanel.add(registerButton);
+	//
+	// // Add horizontal panel to layout
+	// layout.setWidget(3, 0, hPanel);
+	// cellFormatter.setColSpan(3, 0, 3);
+	//
+	// // Create advanced options
+	// Grid advancedOptions = new Grid(1, 1);
+	// advancedOptions.setCellSpacing(6);
+	//
+	// Hyperlink hyperlink = new Hyperlink();
+	// hyperlink.setHTML(LoginFormConstants.HYPERLINK_TEXT);
+	// hyperlink.addClickHandler(new ClickHandler() {
+	//
+	// @Override
+	// public void onClick(ClickEvent event) {
+	// DialogBox dialogBox = createForgotPasswordDialogBox();
+	// dialogBox.setGlassEnabled(true);
+	// dialogBox.setAnimationEnabled(true);
+	// dialogBox.center();
+	// dialogBox.show();
+	// }
+	// });
+	// advancedOptions.setWidget(0, 0, hyperlink);
+	//
+	// // Add advanced options to form in a disclosure panel
+	// DisclosurePanel advancedDisclosure = new DisclosurePanel(LoginFormConstants.ADVANCED_CRITERIA);
+	// advancedDisclosure.setAnimationEnabled(true);
+	// advancedDisclosure.ensureDebugId("disclosurePanel");
+	// advancedDisclosure.setContent(advancedOptions);
+	//
+	// // Add disclorue panel to layout
+	// layout.setWidget(4, 0, advancedDisclosure);
+	// cellFormatter.setColSpan(4, 0, 2);
+	//
+	// // Wrap the contents in a DecoratorPanel
+	// DecoratorPanel decPanel = new DecoratorPanel();
+	// decPanel.setWidget(layout);
+	//
+	// return decPanel;
+	//
+	// }
 
 	protected DialogBox createForgotPasswordDialogBox() {
 		final DialogBox dialogBox = new DialogBox();
